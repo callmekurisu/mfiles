cec61b44ca12778b5559d5f2ccce39b0d310c421
Nigel Christian <callmekurisu@users.noreply.github.com>
2015-08-11 14:17:56 +0900

    Create genesis.m

---
+++ genesis.m
@@ -0,0 +1,46 @@
+clear; clc;clf;
+%plot a circle. plot a second circle with its center anywhere on the edge
+%of the first cirlce. plot remaining cirlces with their centers at all 
+%intersection points.
+circle_plot(0,0,1);
+circle_plot(0,1,1);
+circle_plot(-.86,.5,1);
+circle_plot(.86,.5,1);
+circle_plot(-.86,-.5,1);
+circle_plot(.86,-.5,1);
+circle_plot(0,-1,1);
+%2nd pattern add .86 to x and 1 to y
+circle_plot(0,2,1);
+circle_plot(-1.72,0,1);
+circle_plot(1.72,.0,1);
+circle_plot(-.86,-1.5,1);
+circle_plot(.86,-1.5,1);
+circle_plot(-.86,1.5,1);
+circle_plot(.86,1.5,1);
+circle_plot(0,-2,1);
+%3rd pattern add .86 to x and 1 to y
+circle_plot(0,3,1);
+circle_plot(-1.72,1,1);
+circle_plot(-1.72,-1,1);
+circle_plot(1.72,1,1);
+circle_plot(1.72,-1,1);
+circle_plot(-.86,-2.5,1);
+circle_plot(.86,-2.5,1);
+circle_plot(-.86,2.5,1);
+circle_plot(.86,2.5,1);
+circle_plot(0,-3,1);
+%4th pattern 
+circle_plot(0,4,1);
+circle_plot(-2.58,.5,1);
+circle_plot(-2.58,-.5,1);
+circle_plot(2.58,.5,1);
+circle_plot(2.58,-.5,1);
+circle_plot(-1.72,2,1);
+circle_plot(-1.72,-2,1);
+circle_plot(1.72,2,1);
+circle_plot(1.72,-2,1);
+circle_plot(-.86,-3.5,1);
+circle_plot(.86,-3.5,1);
+circle_plot(-.86,3.5,1);
+circle_plot(.86,3.5,1);
+circle_plot(0,-4,1);
